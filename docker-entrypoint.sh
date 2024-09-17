1dccc5c1ac02560216b66d8b643f9b936983144a
Alex Conner <38727+codatory@users.noreply.github.com>
2024-09-17 16:06:37 -0400

    Initial import of docker files

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+source ~/ansible-venv/bin/activate
+
+# first arg is `-f` or `--some-option`
+# or there are no args
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+	# docker run bash -c 'echo hi'
+	exec bash "$@"
+fi
+
+exec "$@"
